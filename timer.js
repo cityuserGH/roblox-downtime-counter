c549204b0095c26f34a15d9b835b4ec184bb60db
cityuserGH <11913427+cityuserGH@users.noreply.github.com>
2021-11-01 14:48:13 +0100

    Roblox is back up

--- timer.js
+++ timer.js
@@ -1,22 +1,26 @@
 'use strict';
 
+const ROBLOX_IS_DOWN = false;
+const OUTAGE_START_TIME = 1635461700; //seconds
+
 class Timer extends React.Component {
+  
   constructor(props) {
     super(props);
     this.state = this.getTime();
   }
 
   getTime() {
-    const OUTAGE_START_TIME = 1635461700; //seconds
+    if (!ROBLOX_IS_DOWN) return {hours: '00', minutes: '00', seconds: '00'};
+    
     const totalSeconds = Math.floor(Date.now() / 1000) - OUTAGE_START_TIME;
-
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
     return { 
       hours: hours < 10 ? '0' + hours : hours,
       minutes: minutes < 10 ? '0' + minutes : minutes,
       seconds: totalSeconds % 60 < 10 ? '0' + totalSeconds % 60 : totalSeconds % 60
-    }
+    };
   }
 
   tick() {
